fed5e589a150ac1b49b99014b1dbd85aaeb2b572
sceext2 <sceext@foxmail.com>
2017-12-01 02:53:06 +0800

    [tool] init android_hook.tea

---
+++ tool/android_hook/tea/app/src/main/kotlin/config.kt
@@ -0,0 +1,14 @@
+package org.sceext.android_hook.qq.tea
+
+class Config {
+    companion object {
+        // log text prefix
+        const val DEBUG_PREFIX: String = "org.sceext.android_hook.qq.tea"
+
+        const val HOOK_PACKAGE: String = "com.tencent.mobileqq"
+        const val HOOK_CLASS: String = "oicq.wlogin_sdk.tools.cryptor"
+
+        // DEBUG only
+        const val PACKAGE_VERSION: String = "7.2.5 (744)"
+    }
+}
---
+++ tool/android_hook/tea/app/src/main/kotlin/hooks.kt
@@ -0,0 +1,18 @@
+package org.sceext.android_hook.qq.tea
+
+import de.robv.android.xposed.XposedHelpers.findAndHookMethod
+import de.robv.android.xposed.XC_MethodHook
+
+import org.sceext.android_hook.qq.tea.Config
+import org.sceext.android_hook.qq.tea.util.log_debug
+import org.sceext.android_hook.qq.tea.util.print_string
+import org.sceext.android_hook.qq.tea.util.print_string_raw
+
+
+class Hooks(val cl: ClassLoader) {
+    // TODO
+
+    fun init_hooks() {
+        // TODO
+    }
+}
