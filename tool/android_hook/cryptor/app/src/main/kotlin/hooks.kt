2f9228db203a8a0e0e73cf431c1748457a7fe9d2
sceext2 <sceext@foxmail.com>
2017-12-01 20:12:23 +0800

    [tool] android_hook.cryptor: test OK

--- tool/android_hook/cryptor/app/src/main/kotlin/hooks.kt
+++ tool/android_hook/cryptor/app/src/main/kotlin/hooks.kt
@@ -15,16 +15,41 @@ class Hooks(val cl: ClassLoader) {
 
     fun init_hooks() {
         // after: decrypt(byte[], int, int, byte[]) -> byte[]
-        // TODO
+        findAndHookMethod(Config.HOOK_CLASS, cl, DECRYPT, ByteArray::class.java, Int::class.java, Int::class.java, ByteArray::class.java, object: XC_MethodHook() {
+            override fun afterHookedMethod(param: MethodHookParam) {
+                val data = print_bytes(param.args[0] as ByteArray?)
+                val data2 = print_bytes(param.args[3] as ByteArray?)
+                val result = print_bytes(param.result as ByteArray?)
+                log_debug("${DECRYPT}: ${param.args[1] as Int}, ${param.args[2] as Int}, ${data2}, ${data} -> ${result}")
+            }
+        })
 
-        // after: decrypt(byte[], byte[])
-        // TODO
+        // after: decrypt(byte[], byte[]) -> byte[]
+        findAndHookMethod(Config.HOOK_CLASS, cl, DECRYPT, ByteArray::class.java, ByteArray::class.java, object: XC_MethodHook() {
+            override fun afterHookedMethod(param: MethodHookParam) {
+                val data = print_bytes(param.args[0] as ByteArray?)
+                val data2  = print_bytes(param.args[1] as ByteArray?)
+                val result = print_bytes(param.result as ByteArray?)
+                log_debug("${DECRYPT}: ${data2}, ${data} -> ${result}")
+            }
+        })
 
-        // after: encrypt(byte[], byte[])
-        // TODO
+        // after: encrypt(byte[], byte[]) -> byte[]
+        findAndHookMethod(Config.HOOK_CLASS, cl, ENCRYPT, ByteArray::class.java, ByteArray::class.java, object: XC_MethodHook() {
+            override fun afterHookedMethod(param: MethodHookParam) {
+                val data = print_bytes(param.args[0] as ByteArray?)
+                val data2 = print_bytes(param.args[1] as ByteArray?)
+                val result = print_bytes(param.result as ByteArray?)
+                log_debug("${ENCRYPT}: ${data2}, ${data} -> ${result}")
+            }
+        })
 
         // before: enableResultRandom(boolean)
-        // TODO
+        findAndHookMethod(Config.HOOK_CLASS, cl, ENABLE_RESULT_RANDOM, Boolean::class.java, object: XC_MethodHook() {
+            override fun beforeHookedMethod(param: MethodHookParam) {
+                log_debug("${ENABLE_RESULT_RANDOM}: ${param.args[0] as Boolean}")
+            }
+        })
 
         log_debug("hooked.")
     }
