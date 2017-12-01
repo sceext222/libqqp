33448acf366e84bdd40cf3c2a264191176cf400e
sceext2 <sceext@foxmail.com>
2017-12-02 03:14:09 +0800

    [tool] android_hook.qlog: test OK

--- tool/android_hook/qlog/app/src/main/kotlin/hooks.kt
+++ tool/android_hook/qlog/app/src/main/kotlin/hooks.kt
@@ -9,13 +9,17 @@ import org.sceext.android_hook.qq.qlog.util.print_string
 
 
 fun print_string_array(a: Array<String>?): String {
-    // TODO
-    return "TODO"
+    if (a == null) {
+        return "<<null>>"
+    }
+    return " " + a.joinToString(separator="\n")
 }
 
 fun print_throwable(e: Throwable?): String {
-    // TODO
-    return "TODO"
+    if (e == null) {
+        return ""
+    }
+    return e.getStackTrace().joinToString(separator="\n")
 }
 
 
@@ -36,31 +40,120 @@ class Hooks(val cl: ClassLoader) {
 
     fun init_hooks() {
         // before: init(String, String, String, long)
+        findAndHookMethod(Config.HOOK_CLASS, cl, INIT, String::class.java, String::class.java, String::class.java, Long::class.java, object: XC_MethodHook() {
+            override fun beforeHookedMethod(param: MethodHookParam) {
+                val t1 = print_string(param.args[0] as String?)
+                val t2 = print_string(param.args[1] as String?)
+                val t3 = print_string(param.args[2] as String?)
+                log_debug("${INIT}: ${param.args[3] as Long}, ${t1}, ${t2}, ${t3}")
+            }
+        })
 
         // after: isColorLevel() -> boolean
+        findAndHookMethod(Config.HOOK_CLASS, cl, IS_COLOR_LEVEL, object: XC_MethodHook() {
+            override fun afterHookedMethod(param: MethodHookParam) {
+                // always return true
+                param.result = true
+            }
+        })
 
         // after: isDevelopLevel() -> boolean
+        findAndHookMethod(Config.HOOK_CLASS, cl, IS_DEVELOP_LEVEL, object: XC_MethodHook() {
+            override fun afterHookedMethod(param: MethodHookParam) {
+                // always return true
+                param.result = true
+            }
+        })
 
         // before: p(String, String)
+        findAndHookMethod(Config.HOOK_CLASS, cl, P, String::class.java, String::class.java, object: XC_MethodHook() {
+            override fun beforeHookedMethod(param: MethodHookParam) {
+                val t1 = print_string(param.args[0] as String?)
+                val t2 = print_string(param.args[1] as String?)
+                log_debug("${P}: ${t1}, ${t2}")
+            }
+        })
 
         // before: e(String, int, String, Throwable)
+        findAndHookMethod(Config.HOOK_CLASS, cl, E, String::class.java, Int::class.java, String::class.java, Throwable::class.java, object: XC_MethodHook() {
+            override fun beforeHookedMethod(param: MethodHookParam) {
+                print_logs(E, param)
+            }
+        })
 
         // before: w(String, int, String, Throwable)
+        findAndHookMethod(Config.HOOK_CLASS, cl, W, String::class.java, Int::class.java, String::class.java, Throwable::class.java, object: XC_MethodHook() {
+            override fun beforeHookedMethod(param: MethodHookParam) {
+                print_logs(W, param)
+            }
+        })
 
         // before: i(String, int, String, Throwable)
+        findAndHookMethod(Config.HOOK_CLASS, cl, I, String::class.java, Int::class.java, String::class.java, Throwable::class.java, object: XC_MethodHook() {
+            override fun beforeHookedMethod(param: MethodHookParam) {
+                print_logs(I, param)
+            }
+        })
 
         // before: d(String, int, String, Throwable)
+        findAndHookMethod(Config.HOOK_CLASS, cl, D, String::class.java, Int::class.java, String::class.java, Throwable::class.java, object: XC_MethodHook() {
+            override fun beforeHookedMethod(param: MethodHookParam) {
+                print_logs(D, param)
+            }
+        })
 
         // before: doReportLogSelf(int, String, String, boolean)
+        findAndHookMethod(Config.HOOK_CLASS, cl, DO_REPORT_LOG_SELF, Int::class.java, String::class.java, String::class.java, Boolean::class.java, object: XC_MethodHook() {
+            override fun beforeHookedMethod(param: MethodHookParam) {
+                val t1 = print_string(param.args[1] as String?)
+                val t2 = print_string(param.args[2] as String?)
+                log_debug("${DO_REPORT_LOG_SELF}: ${param.args[0] as Int}, ${param.args[3] as Boolean}, ${t1}, ${t2}")
+            }
+        })
 
         // before: syncReportLogSelf(int, String, String, String)
+        findAndHookMethod(Config.HOOK_CLASS, cl, SYNC_REPORT_LOG_SELF, Int::class.java, String::class.java, String::class.java, String::class.java, object: XC_MethodHook() {
+            override fun beforeHookedMethod(param: MethodHookParam) {
+                val t1 = print_string(param.args[1] as String?)
+                val t2 = print_string(param.args[2] as String?)
+                val t3 = print_string(param.args[3] as String?)
+                log_debug("${SYNC_REPORT_LOG_SELF}: ${param.args[0] as Int}, ${t1}, ${t2}, ${t3}")
+            }
+        })
 
         // before: startColorLog(String[])
+        findAndHookMethod(Config.HOOK_CLASS, cl, START_COLOR_LOG, Array<String>::class.java, object: XC_MethodHook() {
+            override fun beforeHookedMethod(param: MethodHookParam) {
+                val text = print_string_array(param.args[0] as Array<String>?)
+                log_debug("${START_COLOR_LOG}: ${text}")
+            }
+        })
 
         // before: endColorLog(String[], int, boolean, String)
+        findAndHookMethod(Config.HOOK_CLASS, cl, END_COLOR_LOG, Array<String>::class.java, Int::class.java, Boolean::class.java, String::class.java, object: XC_MethodHook() {
+            override fun beforeHookedMethod(param: MethodHookParam) {
+                val t1 = print_string_array(param.args[0] as Array<String>?)
+                val t2 = print_string(param.args[3] as String?)
+                log_debug("${END_COLOR_LOG}: ${param.args[1] as Int}, ${param.args[2] as Boolean}, ${t2}, ${t1}")
+            }
+        })
 
         // after: isDebugVersion() -> boolean
+        findAndHookMethod(Config.HOOK_CLASS, cl, IS_DEBUG_VERSION, object: XC_MethodHook() {
+            override fun afterHookedMethod(param: MethodHookParam) {
+                // always return true
+                param.result = true
+            }
+        })
 
         log_debug("hooked.")
+    }
+
+    // before: X(String, int, String, Throwable)
+    fun print_logs(f: String, param: XC_MethodHook.MethodHookParam) {
+        val t1 = print_string(param.args[0] as String?)
+        val t2 = print_string(param.args[2] as String?)
+        val t3 = print_throwable(param.args[3] as Throwable?)
+        log_debug("${f}: ${param.args[1] as Int}, ${t1}, ${t2}, ${t3}")
     }
 }
